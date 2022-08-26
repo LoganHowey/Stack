777382d2bbe9744a23c390981a13b893fd032233
LoganHowey <logan.howey@rocky.edu>
2022-08-26 14:49:19 -0600

    Finished?

--- src/util/Stack.java
+++ src/util/Stack.java
@@ -3,19 +3,23 @@ package util;
 import java.lang.reflect.Array;
 
 public class Stack<Type> {
-    public Type[] data = (Type[]) new Object[10];
+    private int size = 10;
+    public Type[] data = (Type[]) new Object[size];
     private int i = 0;
 
     public void push(Type item) {
+        if (i == size) {
+            throw new ArrayIndexOutOfBoundsException("There is nothing to pop");}
+        else{
         data[i] = item;
         i++;
+        }
     }
 
     public void pop() {
-        if (i == 0){
+        if (i == 0) {
             throw new ArrayIndexOutOfBoundsException("There is nothing to pop");
-        }
-        else {
+        } else {
             i--;
             data[i] = null;
         }
@@ -30,16 +34,30 @@ public class Stack<Type> {
     }
 
     void grow() {
+        Type[] temp = (Type[]) new Object[size+1];
+        for(int i = 0;  i < size; i++){
+             temp[i+1] = data[i];
+        }
+        size++;
+        data = temp;
+
+
 
     }
 
     ;
 
     boolean requiresGrowth() {
-        return false;
+        if (i == size){
+        return true;
+    }
+        else {
+            return false;
+        }
+
     }
 
-    ;
-
-
+    void testing(){
+        size++;
+    }
 }
--- src/util/StackTest.java
+++ src/util/StackTest.java
@@ -26,7 +26,7 @@ public class StackTest {
     }
 
     @Test
-    public void pushMultipleItems(){
+    public void pushMultipleItems() {
         Stack<Object> objectStack = new Stack<>();
         objectStack.push("one");
         objectStack.push("Two");
@@ -38,9 +38,99 @@ public class StackTest {
 
     @Test
             (expected = ArrayIndexOutOfBoundsException.class)
-    public void popEmptyStack(){
+    public void popEmptyStack() {
         Stack<Object> objectStack = new Stack<>();
         objectStack.pop();
         objectStack.pop();
+        System.out.println("hi");
+    }
+
+    @Test
+    public void createFullStack() {
+        Stack<Object> objectStack = new Stack<>();
+        objectStack.push("one");
+        objectStack.push("Two");
+        objectStack.push("Three");
+        objectStack.push("Four");
+        objectStack.push("Five");
+        objectStack.push("Six");
+        objectStack.push("Seven");
+        objectStack.push("Eight");
+        objectStack.push("Nine");
+        objectStack.push("Ten");
+        Assert.assertThat(objectStack.size(), Is.is(10));
+
+    }
+
+    @Test
+            (expected = ArrayIndexOutOfBoundsException.class)
+    public void pushOnFullStack() {
+        Stack<Object> objectStack = new Stack<>();
+        objectStack.push("one");
+        objectStack.push("Two");
+        objectStack.push("Three");
+        objectStack.push("Four");
+        objectStack.push("Five");
+        objectStack.push("Six");
+        objectStack.push("Seven");
+        objectStack.push("Eight");
+        objectStack.push("Nine");
+        objectStack.push("Ten");
+        objectStack.push("Eleven");
+    }
+
+    @Test
+    public void checkSizeGrowth() {
+        Stack<Object> objectStack = new Stack<>();
+        objectStack.push("one");
+        objectStack.push("Two");
+        objectStack.push("Three");
+        objectStack.push("Four");
+        objectStack.push("Five");
+        objectStack.push("Six");
+        objectStack.push("Seven");
+        objectStack.push("Eight");
+        objectStack.push("Nine");
+        objectStack.push("Ten");
+        objectStack.grow();
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            objectStack.push("Eleven");
+        objectStack.grow();
+        objectStack.push("Twelve");
+        Assert.assertThat(objectStack.size(), Is.is(12));
+
+
+    }
+
+    @Test
+    public void stackRequiresGrowth() {
+        Stack<Object> objectStack = new Stack<>();
+        objectStack.push("one");
+        objectStack.push("Two");
+        objectStack.push("Three");
+        objectStack.push("Four");
+        objectStack.push("Five");
+        objectStack.push("Six");
+        objectStack.push("Seven");
+        objectStack.push("Eight");
+        objectStack.push("Nine");
+        objectStack.push("Ten");
+        objectStack.requiresGrowth();
+        Assert.assertThat(objectStack.requiresGrowth(), Is.is(true));
+    }
+
+    @Test
+    public void stackDoesNotRequiresGrowth() {
+        Stack<Object> objectStack = new Stack<>();
+        objectStack.push("one");
+        objectStack.push("Two");
+        objectStack.push("Three");
+        objectStack.push("Four");
+        objectStack.push("Five");
+        objectStack.push("Six");
+        objectStack.push("Seven");
+        objectStack.push("Eight");
+        objectStack.push("Nine");
+        objectStack.requiresGrowth();
+        Assert.assertThat(objectStack.requiresGrowth(), Is.is(false));
     }
 }
